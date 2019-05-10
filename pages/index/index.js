c0b64680a399bce5a57083b5fa226936f8266871
caoyabo <575142603@qq.com>
2019-05-10 15:17:17 +0800

    all

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+const app = getApp();
+
+Page({
+  data: {},
+
+  onLoad() {
+    app.getUserInfo().then(
+      user => {
+        this.setData({
+          user,
+        });
+      },
+      () => {
+        // 获取用户信息失败
+      }
+    );
+  },
+
+  onShow() {
+  }
+})
